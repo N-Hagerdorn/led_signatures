767c6246bd579b30376ec7fda37cdc3e09d3fdb1
N-Hagerdorn <n-hagerdorn@onu.edu>
2024-03-03 15:01:10 -0500

    Configured for use on RPi 4 with Arducam camera

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import socket, select
 from OverheadCamera import OverheadCamera as oc
-
+from picamera2 import Picamera2
 
 #  Define a function to perform the contour detection
 def get_all_contours(grayscale_img):
@@ -13,24 +13,45 @@ def get_all_contours(grayscale_img):
 
 CAM_WIDTH = 1280
 CAM_HEIGHT = 720
+CAM_FOV_WIDTH = 110
+CAM_FOV_HEIGHT = 95
 
 # Define the overhead camera object that performs coordinate transformations
-cam = oc(image_size=(CAM_WIDTH, CAM_HEIGHT), midfield_offset=0, sideline_offset=2, height=3.5)
+cam = oc(image_size=(CAM_WIDTH, CAM_HEIGHT), midfield_offset=0, sideline_offset=2, height=53/12)
 
 # Configure the camera
-vid = cv2.VideoCapture(0)
-vid.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
-vid.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
-vid.set(cv2.CAP_PROP_FPS, 2)
-vid.set(cv2.CAP_PROP_EXPOSURE, -8)
+isRPi = True
+if isRPi:
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_preview_configuration(main={'format': 'XRGB8888', 'size': (CAM_WIDTH, CAM_HEIGHT)}))
+    
+    picam2.start()
+    print('Configuring exposure...')
+    exposure = picam2.capture_metadata()['ExposureTime']
+    print(exposure)
+    
+    picam2.stop()
+    picam2.set_controls({'ExposureTime': int(exposure / 10)})
+    
+    picam2.start()
+else:
+    vid = cv2.VideoCapture(0)
+    vid.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
+    vid.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
+    vid.set(cv2.CAP_PROP_FPS, 2)
+    vid.set(cv2.CAP_PROP_EXPOSURE, -8)
 
+    if not vid.isOpened():
+        print('Cannot open camera...')
+        exit()
 
+print('Starting server...')
 host = socket.gethostname()
 print(host)
 port = 5000
 
-server_socket = socket.socket()
-server_socket.bind((host, port))
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('', port))
 server_socket.listen(1)
 conn, address = server_socket.accept()
 print("Connection from: " + str(address))
@@ -39,7 +60,10 @@ while True:
 
 
     # Capture video from the webcam
-    ret, frame = vid.read()
+    if isRPi:
+        frame = picam2.capture_array()
+    else:
+        _, frame = vid.read()
 
     # the 'q' button is set as the
     # quitting button you may use any
@@ -51,13 +75,14 @@ while True:
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Identify bright spots in the image, such as LEDs and put them in a binary image
-    thresh, binary_m = cv2.threshold(gray_frame, 230, 255, cv2.THRESH_BINARY)
+    _, binary_m = cv2.threshold(gray_frame, 230, 255, cv2.THRESH_BINARY)
 
     # Get all contours (boundaries of the white spots) in the binary image
     contours = get_all_contours(binary_m)
 
+    circles = None
     # Try to identify circles in the image. This doesn't work very well
-    circles = cv2.HoughCircles(gray_frame, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2=15, minRadius=0, maxRadius=40)
+    #circles = cv2.HoughCircles(gray_frame, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2=15, minRadius=0, maxRadius=40)
 
     # Get a list of the center points of all LEDs
     LEDs = []
@@ -86,49 +111,59 @@ while True:
     '''
 
     # Split the list of captured LEDs into transmissible chunks
-    split_LEDs = [LEDs[i:i+20] for i in range(0, len(LEDs), 20)]
+    split_LEDs = [LEDs[i:i+50] for i in range(0, len(LEDs), 50)]
     LEDs = []
 
-    '''num_packets = len(data)
-    packet = str(num_packets)
-    for i in range(num_packets):
-        packet += str(data[i])
-        print('Sending ' + packet)
-        conn.send(packet.encode())
-        packet = ''
-    '''
-    #response = conn.recv(1024).decode()
-    #if response != 'OK':
-        #print('Bad or no response')
-        #break
+    num_packets = len(split_LEDs)
+    print("Num packets: " + str(num_packets))
+    data = ''
+    
+    
+    timeout = 10  # in seconds
 
-    timeout = 1  # in seconds
+    for i in range(num_packets):
+        
+        ready_sockets, _, _ = select.select(
+            [conn], [], [], timeout
+        )
+        if ready_sockets:
+            response = conn.recv(256).decode()
+            if response != 'OK':
+                print('Bad response from client')            
+                break
+
+        else:
+            print('No response')
+            break
+        
+        
+        data = '['
+        for point in split_LEDs[i]:
+            formatted_point = '({X:.2f}, {Y:.2f})'.format(X=point[0], Y=point[1])
+            data += formatted_point + ', '
+
+        data = data[0:-2] + ']'
+
+        print('Sending ' + data)
+        conn.send(data.encode())
+        
+        
+
     ready_sockets, _, _ = select.select(
         [conn], [], [], timeout
     )
     if ready_sockets:
-        response = conn.recv(1024).decode()
+        response = conn.recv(256).decode()
         if response != 'OK':
+            print('Bad response from client')            
             break
-
-        num_packets = len(split_LEDs)
-        print("Num packets: " + str(num_packets))
-        data = str(num_packets) + '['
-        for i in range(num_packets):
-
-            for point in split_LEDs[i]:
-                formatted_point = '({X:.2f}, {Y:.2f})'.format(X=point[0], Y=point[1])
-                data += formatted_point + ', '
-
-            data = data[0:-2] + ']'
-
-            print('Sending ' + data)
-            conn.send(data.encode())
-            packet = ''
+        print(response)
     else:
         print('No response')
-
-
+        break
+    
+    data = 'EOT'
+    conn.send(data.encode())
 
     # Draw all circles
     if circles is not None:
@@ -147,12 +182,18 @@ while True:
 
     cv2.imshow('frame', frame)
 
-# After the loop release the cap object
-vid.release()
+
+# Close the TCP socket
+conn.close()
+
+print('TCP socket closed...')
+
 # Destroy all the windows
 cv2.destroyAllWindows()
 
 
-conn.close()
 
-print('Closed...')
+# After the loop release the cap object
+vid.release()
+# After the loop release the cap object
+vid.release()
