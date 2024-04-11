bba6112349eccfe2dd70753be869a78de9684984
N-Hagerdorn <n-hagerdorn@onu.edu>
2024-04-10 23:01:00 -0400

    Added recording feature to save an mp4 of the session.

--- OverheadCamera.py
+++ OverheadCamera.py
@@ -1,9 +1,5 @@
 import math
 
-# Camera field of view in degrees
-FOV_HORIZONTAL = 65
-FOV_VERTICAL = 37
-
 # Field size in feet
 FIELD_LENGTH = 90
 FIELD_WIDTH = 46
@@ -11,28 +7,34 @@ FIELD_WIDTH = 46
 
 class OverheadCamera:
 
-    def __init__(self, image_size, midfield_offset, sideline_offset, height):
+    def __init__(self, field_of_view, phi, image_size, midfield_offset, sideline_offset, height):
+        self.field_of_view = field_of_view
+
         self.image_size = image_size
 
         self.x_offset = midfield_offset
         self.y_offset = -sideline_offset
         self.z_offset = height
 
-    def pixels_to_spherical(self, x, y):
+        # Calculate the vertical angle (theta) and horizontal angle (phi) of the center of the camera's view
+        max_y = sideline_offset + FIELD_WIDTH
+        vert_fov_max = math.atan(max_y/height)
+        self.cam_theta = 180 - vert_fov_max + (self.field_of_view[1] / 2)
+        self.cam_phi = phi
+
+    def pixelsToSpherical(self, x, y):
         # phi indicates a horizontal angle from the x axis
         # theta indicates the angle from the vertical, with 0 degrees pointing straight up
-        phi_cam = 90        # 90 degree is a baseline but will be procedurally changed later
-        theta_cam = 135     # 90 degree means the camera is pointed horizontally
 
-        phi = phi_cam + FOV_HORIZONTAL * (0.5 - x / self.image_size[0])
-        theta = theta_cam + FOV_VERTICAL * (y / self.image_size[1] - 0.5)
+        phi = self.cam_phi + self.field_of_view[0] * (0.5 - x / self.image_size[0])
+        theta = self.cam_theta + self.field_of_view[1] * (y / self.image_size[1] - 0.5)
         radius = -self.z_offset / math.cos(theta * math.pi / 180)
 
         spherical_point = (radius, theta, phi)
 
         return spherical_point
 
-    def spherical_to_cartesian(self, spherical_point):
+    def sphericalToCartesian(self, spherical_point):
         z = -self.z_offset
         radius, theta, phi = spherical_point
         x = radius * math.sin(theta * math.pi / 180) * math.cos(phi * math.pi / 180) + FIELD_LENGTH / 2 - self.x_offset
@@ -41,3 +43,6 @@ class OverheadCamera:
         cartesian_point = (x, y, z)
 
         return cartesian_point
+
+    def pixelsToCartesian(self, x, y):
+        return self.sphericalToCartesian(self.pixelsToSpherical(x, y))
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import cv2
 import numpy as np
-import socket, select, time
+import socket, select, time, os, datetime, shutil
 
 import botDetector, botPatterns
 from OverheadCamera import OverheadCamera as oc
 from botDetector import *
 
 # Run parameters
-RUN_SERVER = False   # Set to True to run the server communication with the GUI client
-IS_RPI = False       # Set to True for the Raspberry Pi, False for a Windows computer (for testing only)
+RUN_SERVER = False      # Will run a server and wait for a client connection if True
+IS_RPI = False          # Set to True for the Raspberry Pi, False to test on a Windows computer
+DISPLAY = True          # Will only open a window to view the camera frames if this is True
+SAVE_FRAME_RATE = 4     # Frame rate to save captured images for later viewing. Will not save if set to 0 or negative.
 
 
 #  Define a function to perform the contour detection
@@ -18,19 +20,40 @@ def get_all_contours(grayscale_img):
     return contours
 
 
-CAM_WIDTH = 4656 # Width of the camera frame in pixels
-CAM_HEIGHT = 3496 # Height of the camera frame in pixels
-CAM_FOV_WIDTH = 120 # Width of the camera frame in degrees, also called horizontal field of view
-CAM_FOV_HEIGHT = 95 # Height of the camera frame in degrees, also called vertical field of view
+def makeVideo(name, image_folder):
 
-# Define the overhead camera object that performs coordinate transformations
-cam = oc(image_size=(CAM_WIDTH, CAM_HEIGHT), midfield_offset=0, sideline_offset=6 + 8/12, height=217/12)
+    images = [img for img in sorted(os.listdir(image_folder)) if img.endswith('.jpg')]
+    if len(images) < 1:
+        return
+
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video = cv2.VideoWriter(name + '.mp4', fourcc, SAVE_FRAME_RATE, (width, height))
+
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+
+    video.release()
+
+
+CAM_WIDTH = 4656        # Width of the camera frame in pixels
+CAM_HEIGHT = 3496       # Height of the camera frame in pixels
+CAM_FOV_WIDTH = 120     # Width of the camera frame in degrees, also called horizontal field of view
+CAM_FOV_HEIGHT = 95     # Height of the camera frame in degrees, also called vertical field of view
 
 # Configure the camera
 if IS_RPI:
+
     # Pi-only module for operating the camera
     # picamera2 does not need to be installed to run on a non-RPi system
     from picamera2 import Picamera2
+
+    CAM_WIDTH = 4656
+    CAM_HEIGHT = 3496
+    CAM_FOV_WIDTH = 120
+    CAM_FOV_HEIGHT = 95
 
     # Set up the Raspberry Pi webcam
     picam2 = Picamera2()
@@ -41,109 +64,192 @@ if IS_RPI:
     exposure = picam2.capture_metadata()['ExposureTime']
     print(exposure)
 
-    exposure_dimming = 5 # Higher dimming makes for a dimmer picture.
-    
+    # Factor to adjust exposure time
+    # 1/12 seems to work
+    exposure_factor = 1/12
+
+    # Stop the webcam and reduce exposure time, then restart the webcam
     picam2.stop()
-    picam2.set_controls({'ExposureTime': int(exposure / exposure_dimming)})
+    picam2.set_controls({'ExposureTime': int(exposure * exposure_factor)})
     
     picam2.start()
+
 else:
+
+    # Typical settings for a widescreen laptop webcam
+    CAM_WIDTH = 1280
+    CAM_HEIGHT = 720
+    CAM_FOV_WIDTH = 65
+    CAM_FOV_HEIGHT = 37
+
+    # Limit the camera exposure to detect LEDs while filtering out other light sources
+    # -8 seems to work for testing
+    exposure_factor = 0#-8
 
     # Set up the default Windows webcam
     vid = cv2.VideoCapture(0)
     vid.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
     vid.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
-    #vid.set(cv2.CAP_PROP_FPS, 2)
-    vid.set(cv2.CAP_PROP_EXPOSURE, -8)#-8)
+    vid.set(cv2.CAP_PROP_EXPOSURE, exposure_factor)
 
     if not vid.isOpened():
         print('Cannot open camera...')
         exit()
 
+# Define the overhead camera object that performs coordinate transformations
+# Use feet for the height and offset measurements to ensure the output of the algorithm is also in feet
+cam = oc(
+    field_of_view=(CAM_FOV_WIDTH, CAM_FOV_HEIGHT),
+    phi=90,
+    image_size=(CAM_WIDTH, CAM_HEIGHT),
+    midfield_offset=0,
+    sideline_offset=6 + 8 / 12,
+    height=217 / 12
+)
+
+
+# Start the TCP server
 if RUN_SERVER:
     print('Starting server...')
+
     host = socket.gethostname()
-    print(host)
     port = 5000
 
+    # Open the TCP socket at the given port
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('', port))
     server_socket.listen(1)
+    print('Listening for TCP session requests at ' + host + ':' + str(port))
+
     conn, address = server_socket.accept()
-    print("Connection from: " + str(address))
+    print('Accepting TCP session from ' + str(address))
 
-while True:
 
-    # Capture video from the webcam
-    if IS_RPI:
-        frame = picam2.capture_array()
-    else:
-        _, frame = vid.read()
+def main():
 
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Get a unique name for the recording of the session
+    session_name = 'recording_' + datetime.datetime.now().strftime('%Y_%m_%d__%H_%M_%S')
 
-    # Convert frame to grayscale
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Determine whether to record the session based on the given frame rate
+    record = False
+    frame_interval = math.inf
+    if SAVE_FRAME_RATE > 0:
+        frame_interval = 1 / SAVE_FRAME_RATE
+        record = True
 
-    # Identify bright spots in the image, such as LEDs and put them in a binary image
-    _, binary_m = cv2.threshold(gray_frame, 230, 255, cv2.THRESH_BINARY)
+        # If the application needs to record, make an empty folder in which to save images
+        if os.path.exists(session_name):
+            shutil.rmtree(session_name)
+        os.makedirs(session_name)
 
-    # Get all contours (boundaries of the white spots) in the binary image
-    contours = get_all_contours(binary_m)
+    # Get the start time of the session
+    start = time.time()
+    mark = start
 
-    # Get a list of the center points of all LEDs
-    LEDs = []
+    # Main loop
+    while True:
 
-    # Print each contour on the original frame
-    for contour in contours:
+        # Capture a frame from the webcam
+        if IS_RPI:
+            frame = picam2.capture_array()
+        else:
+            _, frame = vid.read()
 
-        M = cv2.moments(contour)
-        if M["m00"] != 0 and cv2.contourArea(contour) > 1:
+        # If q is pressed, stop the main loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-            # Draw the contour
-            cv2.drawContours(frame, [contour], -1, (255, 255, 0), 3)
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-            cv2.circle(frame, (cX, cY), 4, (0, 255, 255), -1)
+        # Convert frame to grayscale
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            radius, theta, phi = cam.pixels_to_spherical(cX, cY)
-            x, y, z = cam.spherical_to_cartesian((radius, theta, phi))
+        # Identify bright spots in the image such as LEDs and put them in a binary image
+        _, binary_m = cv2.threshold(gray_frame, 230, 255, cv2.THRESH_BINARY)
 
-            LEDs.append((x, y))
-            cv2.putText(frame, 'LED position: {:.2f}, {:.2f}'.format(x, y), (cX, cY), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        # Get all contours (boundaries of the white spots) in the binary image
+        contours = get_all_contours(binary_m)
 
-    groups = botDetector.groupNearbyPoints(LEDs, 1)
-    X = []
-    for group in groups:
-
-        if len(group) < 1:
-            continue
-
-        score = botDetector.detectShape(group, botPatterns.getPattern('X'))
-        if score < math.inf:
-
-            print('Matching score: ' + str(score))
-
-    LEDs = X
-
-    # If the server is running, transmit the points to the client
-    if RUN_SERVER:
-
-        # Split the list of captured LEDs into transmissible chunks
-        split_LEDs = [LEDs[i:i+50] for i in range(0, len(LEDs), 50)]
+        # Get a list of the center points of all LEDs
         LEDs = []
 
-        num_packets = len(split_LEDs)
-        print("Num packets: " + str(num_packets))
-        data = ''
+        # Print each contour on the original frame
+        for contour in contours:
 
-        timeout = 10  # in seconds
+            # Get the center point of the contour
+            M = cv2.moments(contour)
+            if M["m00"] != 0 and cv2.contourArea(contour) > 1:
 
-        for i in range(num_packets):
+                # Draw the contour
+                cv2.drawContours(frame, [contour], -1, (255, 255, 0), 3)
+
+                # Find and draw the contour's center
+                cX = int(M['m10'] / M['m00'])
+                cY = int(M['m01'] / M['m00'])
+
+                cv2.circle(frame, (cX, cY), 4, (0, 255, 255), -1)
+
+                # Convert the pixel coordinates to field coordinates
+                x, y, z = cam.pixelsToCartesian(cX, cY)
+                LEDs.append((x, y))
+
+                cv2.putText(frame, 'LED position: {:.2f}, {:.2f}'.format(x, y), (cX, cY), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+        if record:
+            # If the current time exceeds the time at which the next frame should be captured, save the current frame
+            now = time.time()
+            if now - mark >= 0:
+                mark = mark + frame_interval
+
+                cv2.imwrite(session_name + '/' + str(now) + '.jpg', frame)
+
+        groups = botDetector.groupNearbyPoints(LEDs, 1)
+        X = []
+        for group in groups:
+
+            if len(group) < 1:
+                continue
+
+            score = botDetector.detectShape(group, botPatterns.getPattern('X'))
+            if score < math.inf:
+
+                print('Matching score: ' + str(score))
+
+        LEDs = X
+
+        # If the server is running, transmit the points to the client
+        if RUN_SERVER:
+
+            # Split the list of captured LEDs into transmissible chunks
+            split_LEDs = [LEDs[i:i+50] for i in range(0, len(LEDs), 50)]
+
+            num_packets = len(split_LEDs)
+            print("Num packets: " + str(num_packets))
+
+            timeout = 10  # in seconds
+
+            for i in range(num_packets):
+
+                ready_sockets, _, _ = select.select(
+                    [conn], [], [], timeout
+                )
+                if ready_sockets:
+                    response = conn.recv(256).decode()
+                    if response != 'OK':
+                        print('Bad response from client')
+                        break
+
+                else:
+                    print('No response')
+                    break
+
+                data = '['
+                for point in split_LEDs[i]:
+                    formatted_point = '({X:.2f}, {Y:.2f})'.format(X=point[0], Y=point[1])
+                    data += formatted_point + ', '
+
+                data = data[0:-2] + ']'
+
+                print('Sending ' + data)
+                conn.send(data.encode())
 
             ready_sockets, _, _ = select.select(
                 [conn], [], [], timeout
@@ -153,50 +259,36 @@ while True:
                 if response != 'OK':
                     print('Bad response from client')
                     break
-
+                print(response)
             else:
                 print('No response')
                 break
 
-            data = '['
-            for point in split_LEDs[i]:
-                formatted_point = '({X:.2f}, {Y:.2f})'.format(X=point[0], Y=point[1])
-                data += formatted_point + ', '
-
-            data = data[0:-2] + ']'
-
-            print('Sending ' + data)
+            data = 'EOT'
             conn.send(data.encode())
 
-        ready_sockets, _, _ = select.select(
-            [conn], [], [], timeout
-        )
-        if ready_sockets:
-            response = conn.recv(256).decode()
-            if response != 'OK':
-                print('Bad response from client')
-                break
-            print(response)
-        else:
-            print('No response')
-            break
+        if DISPLAY:
+            cv2.imshow('frame', frame)
 
-        data = 'EOT'
-        conn.send(data.encode())
-
-    cv2.imshow('frame', frame)
-
-
-if RUN_SERVER:
     # Close the TCP socket
-    conn.close()
+    if RUN_SERVER:
+        conn.close()
+        print('TCP socket closed...')
 
-    print('TCP socket closed...')
+    # Destroy the display window for the live view
+    if DISPLAY:
+        cv2.destroyAllWindows()
 
-# Destroy all the windows
-cv2.destroyAllWindows()
+    if IS_RPI:
+        # Stop recording from the Pi's camera
+        picam2.stop()
+    else:
+        # Stop recording from the Windows camera
+        vid.release()
 
-# After the loop release the cap object
-vid.release()
-# After the loop release the cap object
-vid.release()
+    if record:
+        makeVideo(name=session_name, image_folder=session_name)
+
+
+if __name__ == '__main__':
+    main()
